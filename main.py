ac76baf55c5d15dc5157a06e3732195905abf203
NewArk81 <751247667@qq.com>
2025-11-28 18:21:52 +0800

    update 1.0.9

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 async def custom_swagger_ui_html():
     return get_swagger_ui_html(
         openapi_url=app.openapi_url,
-        title=app.title + " - Swagger UI",
+        title=app.title + " - 开发者文档",
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
         swagger_js_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.29.0/swagger-ui-bundle.js",
         swagger_css_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.29.0/swagger-ui.css",
